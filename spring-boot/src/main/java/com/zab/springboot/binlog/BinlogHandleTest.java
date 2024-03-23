4aa25eca1016ab41bd4b891355f69ffad195379e
zab <635590867@qq.com>
2024-03-23 14:47:27 +0800

    1、下载canal1.1.7版本。2、保证jdk版本1.8,11以上用不了。3、配置mysql可以监听binlog。4、启动canal。5、做增删改查操作，代码可以输出变化

---
+++ spring-boot/src/main/java/com/zab/springboot/binlog/BinlogHandleTest.java
@@ -0,0 +1,90 @@
+package com.zab.springboot.binlog;
+
+import com.alibaba.otter.canal.client.CanalConnector;
+import com.alibaba.otter.canal.client.CanalConnectors;
+import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.alibaba.otter.canal.protocol.Message;
+import java.net.InetSocketAddress;
+import java.util.List;
+
+public class BinlogHandleTest {
+
+    public static void main(String args[]) {
+        // 创建链接
+        CanalConnector connector = CanalConnectors.newSingleConnector(
+                new InetSocketAddress("127.0.0.1",
+                        11111), "example", "", "");
+        int batchSize = 1000;
+        try {
+            connector.connect();
+            connector.subscribe(".*\\..*");
+            connector.rollback();
+            while (true) {
+                // 获取指定数量的数据
+                Message message = connector.getWithoutAck(batchSize);
+                long batchId = message.getId();
+                int size = message.getEntries().size();
+                if (batchId == -1 || size == 0) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    // 处理数据
+                    printEntry(message.getEntries());
+                }
+                // 提交确认，表示本批数据已正确处理完毕
+                connector.ack(batchId);
+            }
+        } finally {
+            connector.disconnect();
+        }
+    }
+
+    private static void printEntry(List<CanalEntry.Entry> entrys) {
+        for (CanalEntry.Entry entry : entrys) {
+            if (entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONBEGIN ||
+                    entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONEND) {
+                continue;
+            }
+
+            CanalEntry.RowChange rowChage = null;
+            try {
+                rowChage = CanalEntry.RowChange.parseFrom(entry.getStoreValue());
+            } catch (Exception e) {
+                throw new RuntimeException("ERROR ## parser of eromanga-event has an error , data:" + entry.toString(),
+                        e);
+            }
+
+            CanalEntry.EventType eventType = rowChage.getEventType();
+            System.out.println(String.format("binlog[%s:%s] , name[%s,%s] , eventType : %s",
+                    entry.getHeader().getLogfileName(),
+                    entry.getHeader().getLogfileOffset(),
+                    entry.getHeader().getSchemaName(),
+                    entry.getHeader().getTableName(),
+                    eventType));
+
+            for (CanalEntry.RowData rowData : rowChage.getRowDatasList()) {
+                if (eventType == CanalEntry.EventType.DELETE) {
+                    printColumn(rowData.getBeforeColumnsList());
+                } else if (eventType == CanalEntry.EventType.INSERT) {
+                    printColumn(rowData.getAfterColumnsList());
+                } else {
+                    // UPDATE操作
+                    System.out.println("-------> before");
+                    printColumn(rowData.getBeforeColumnsList());
+                    System.out.println("-------> after");
+                    printColumn(rowData.getAfterColumnsList());
+                }
+            }
+        }
+    }
+
+    private static void printColumn(List<CanalEntry.Column> columns) {
+        for (CanalEntry.Column column : columns) {
+            System.out.println(column.getName() + " : " + column.getValue() +
+                    "    update=" + column.getUpdated());
+        }
+    }
+}
