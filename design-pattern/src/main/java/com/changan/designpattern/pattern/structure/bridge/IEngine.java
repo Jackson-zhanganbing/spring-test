aac61d7776c177b316fbce2e9ec25c8a75109fa6
zab <635590867@qq.com>
2024-11-10 12:28:03 +0800

    桥接模式

---
+++ design-pattern/src/main/java/com/changan/designpattern/pattern/structure/bridge/IEngine.java
@@ -0,0 +1,14 @@
+package com.changan.designpattern.pattern.structure.bridge;
+
+/**
+ * 引擎接口
+ *
+ * @author zab
+ * @date 2024/11/10 12:15
+ */
+public interface IEngine {
+    /**
+     * 发动机描述
+     */
+    void desc();
+}
