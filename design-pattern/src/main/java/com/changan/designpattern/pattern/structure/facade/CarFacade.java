9d00e1c47b3fccf637e305f4b25f0cf3283ff969
zab <635590867@qq.com>
2024-11-11 22:54:47 +0800

    门面模式

---
+++ design-pattern/src/main/java/com/changan/designpattern/pattern/structure/facade/CarFacade.java
@@ -0,0 +1,27 @@
+package com.changan.designpattern.pattern.structure.facade;
+
+/**
+ * 车辆 门面模式
+ *
+ * @author zab
+ * @date 2024/11/11 22:51
+ */
+public class CarFacade {
+    private Engine engine;
+    private FuelSystem fuelSystem;
+    private IgnitionSystem ignitionSystem;
+
+    public CarFacade() {
+        engine = new Engine();
+        fuelSystem = new FuelSystem();
+        ignitionSystem = new IgnitionSystem();
+    }
+
+    public void startCar() {
+        System.out.println("Starting the car...");
+        fuelSystem.pump();
+        ignitionSystem.ignite();
+        engine.run();
+        System.out.println("Car is now running.");
+    }
+}
---
+++ design-pattern/src/main/java/com/changan/designpattern/pattern/structure/facade/Engine.java
@@ -0,0 +1,13 @@
+package com.changan.designpattern.pattern.structure.facade;
+
+/**
+ * 引擎
+ *
+ * @author zab
+ * @date 2024/11/11 22:48
+ */
+public class Engine {
+    public void run(){
+        System.out.println("引擎动起来");
+    }
+}
