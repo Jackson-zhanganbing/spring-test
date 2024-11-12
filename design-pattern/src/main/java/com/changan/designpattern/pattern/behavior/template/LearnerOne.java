cf61a96a6f8e83be6eca1e35130aef17c06563f2
zab <635590867@qq.com>
2024-11-12 21:25:25 +0800

    模板方法模式

---
+++ design-pattern/src/main/java/com/changan/designpattern/pattern/behavior/template/LearnerOne.java
@@ -0,0 +1,29 @@
+package com.changan.designpattern.pattern.behavior.template;
+
+/**
+ * 学习者1
+ *
+ * @author zab
+ * @date 2024/11/12 21:22
+ */
+public class LearnerOne extends KnowledgeLearner{
+    @Override
+    public void visitClass() {
+        System.out.println("学习者1听课中···");
+    }
+
+    @Override
+    public void review() {
+        System.out.println("学习者1复习中···");
+    }
+
+    @Override
+    public void practice() {
+        System.out.println("学习者1练习中···");
+    }
+
+    @Override
+    public void teach() {
+        System.out.println("学习者1给别人讲课中···");
+    }
+}
---
+++ design-pattern/src/main/java/com/changan/designpattern/pattern/behavior/template/Test.java
@@ -0,0 +1,19 @@
+package com.changan.designpattern.pattern.behavior.template;
+
+/**
+ * 模板方法设计模式测试
+ *
+ * @author zab
+ * @date 2024/11/12 21:24
+ */
+public class Test {
+    public static void main(String[] args) {
+        LearnerOne learnerOne = new LearnerOne();
+        LearnerTwo learnerTwo = new LearnerTwo();
+
+        learnerOne.learn();
+        System.out.println("=============");
+        learnerTwo.learn();
+
+    }
+}
