67a56c668e56f06a6214d9ab6fdc0f29d25cdd99
zab <635590867@qq.com>
2025-01-08 23:38:22 +0800

    迭代器、解释器、命令模式串讲词

--- design-pattern/src/main/java/com/zab/designpattern/pattern/behavior/interpreter/Test.java
+++ design-pattern/src/main/java/com/zab/designpattern/pattern/behavior/interpreter/Test.java
@@ -1,5 +1,7 @@
 package com.zab.designpattern.pattern.behavior.interpreter;
 
+import java.util.List;
+
 /**
  * 解释器模式测试
  *
@@ -9,7 +11,6 @@ package com.zab.designpattern.pattern.behavior.interpreter;
 public class Test {
     public static void main(String[] args) {
         String input = "3 5 + 4 -";
-
         ExpressionParser expressionParser = new ExpressionParser();
         Expression parse = expressionParser.parse(input);
         System.out.println(parse.interpret());
